6c0efec3ddb684b25f7d824b97006c46c7da7122
One <43485962+c-git@users.noreply.github.com>
2024-05-25 00:29:31 -0400

    07-09
    Checked error type from solution as it wasn't used

--- exercises/07_threads/09_bounded/src/lib.rs
+++ exercises/07_threads/09_bounded/src/lib.rs
@@ -1,40 +1,75 @@
 // TODO: Convert the implementation to use bounded channels.
 use crate::data::{Ticket, TicketDraft};
 use crate::store::{TicketId, TicketStore};
-use std::sync::mpsc::{Receiver, Sender};
+use std::sync::mpsc::{self, Receiver, Sender, TrySendError};
 
 pub mod data;
 pub mod store;
 
 #[derive(Clone)]
 pub struct TicketStoreClient {
-    sender: todo!(),
+    sender: mpsc::SyncSender<Command>,
+}
+#[derive(Debug, thiserror::Error)]
+#[error("Channel is Full")]
+pub struct ErrorChannelFull<T: Send> {
+    input: T,
 }
 
 impl TicketStoreClient {
-    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, todo!()> {
-        todo!()
+    pub fn insert(&self, draft: TicketDraft) -> Result<TicketId, ErrorChannelFull<TicketDraft>> {
+        let (tx, rx) = mpsc::sync_channel(1);
+        if let Err(e) = self.sender.try_send(Command::Insert {
+            draft,
+            response_channel: tx,
+        }) {
+            match e {
+                TrySendError::Full(d) | TrySendError::Disconnected(d) => match d {
+                    Command::Insert {
+                        draft,
+                        response_channel: _,
+                    } => return Err(ErrorChannelFull { input: draft }),
+                    _ => unreachable!(),
+                },
+            }
+        }
+        Ok(rx.recv().unwrap())
     }
 
-    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Result<Option<Ticket>, ErrorChannelFull<TicketId>> {
+        let (tx, rx) = mpsc::sync_channel(1);
+        if let Err(e) = self.sender.try_send(Command::Get {
+            id,
+            response_channel: tx,
+        }) {
+            match e {
+                TrySendError::Full(x) | TrySendError::Disconnected(x) => match x {
+                    Command::Insert { .. } => unreachable!(),
+                    Command::Get {
+                        id,
+                        response_channel: _,
+                    } => return Err(ErrorChannelFull { input: id }),
+                },
+            }
+        }
+        Ok(rx.recv().unwrap())
     }
 }
 
 pub fn launch(capacity: usize) -> TicketStoreClient {
-    todo!();
-    std::thread::spawn(move || server(receiver));
-    todo!()
+    let (tx, rx) = mpsc::sync_channel(capacity);
+    std::thread::spawn(move || server(rx));
+    TicketStoreClient { sender: tx }
 }
 
 enum Command {
     Insert {
         draft: TicketDraft,
-        response_channel: todo!(),
+        response_channel: mpsc::SyncSender<TicketId>,
     },
     Get {
         id: TicketId,
-        response_channel: todo!(),
+        response_channel: mpsc::SyncSender<Option<Ticket>>,
     },
 }
 
@@ -47,14 +82,14 @@ pub fn server(receiver: Receiver<Command>) {
                 response_channel,
             }) => {
                 let id = store.add_ticket(draft);
-                todo!()
+                let _ = response_channel.try_send(id);
             }
             Ok(Command::Get {
                 id,
                 response_channel,
             }) => {
-                let ticket = store.get(id);
-                todo!()
+                let ticket = store.get(id).cloned();
+                let _ = response_channel.try_send(ticket);
             }
             Err(_) => {
                 // There are no more senders, so we can safely break
